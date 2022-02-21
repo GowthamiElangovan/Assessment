be5014427c5632562e248dd1be4e25dfcf910024
GowthamiElangovan <98298791+GowthamiElangovan@users.noreply.github.com>
2022-02-21 12:46:37 +0530

    Add files via upload

---
+++ HtmlPrograms/build/sample.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    var name="Gowthami";
+    function printName(){
+        alert("My Name is : "+name);
+        console.log("My Name is : "+name);
+    }
+    printName();
+}
+var yourFunction=myFunction();
+yourFunction();
